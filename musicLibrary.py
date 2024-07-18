b1ea79c326b4f7843150623c1787e843dc18ffc3
Yash-Baviskar <89994161+Yash-Baviskar@users.noreply.github.com>
2024-07-18 12:48:23 +0530

    Add files via upload
    DESIGNED AND IMPLEMENTED LEO, A VOICE-DRIVEN ASSISTANT USING SPEECH RECOGNITION AND TEXT-TO-
    SPEECH TECHNOLOGIES.
    ENABLED FUNCTIONALITIES LIKE WEB BROWSING, MUSIC PLAYBACK, AND NEWS RETRIEVAL, ENHANCING USER
    INTERACTION THROUGH VOICE COMMANDS.

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI" ,
+    "Dragon": "https://www.youtube.com/watch?v=W0DM5lcj6mw&list=PLW8DYY9GtNCuBaytJpU3Cwn3lyVvY28cP",
+    "unstoppable": "https://www.youtube.com/watch?v=YaEG2aWJnZ8&list=PLW8DYY9GtNCuBaytJpU3Cwn3lyVvY28cP&index=3",
+    "sajni" :" https://www.youtube.com/watch?v=XJAMVPQWvGY"
+    
+    
+}
